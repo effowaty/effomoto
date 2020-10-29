ee3ed775a2f1536ee0894dbf743289b1d9b41499
unknown <kamilwoszczyk@gmail.com>
2020-10-29 16:58:48 +0100

    KaWoszczyk #0 Initail commit - app structure

---
+++ effomoto/src/main/java/com/effomoto/EffomotoApplication.java
@@ -0,0 +1,13 @@
+package com.effomoto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EffomotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EffomotoApplication.class, args);
+	}
+
+}
